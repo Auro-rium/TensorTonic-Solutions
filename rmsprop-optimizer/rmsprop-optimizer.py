1fefcaa6730a84527ad96bbc8a58c633ce08ac1f
Aurorium Nexus <ishtrivedi18@gmail.com>
2026-09-18 12:23:14 +0530

    Solve rmsprop-optimizer

---
+++ rmsprop-optimizer/rmsprop-optimizer.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def rmsprop_step(
+    w: list,
+    g: list,
+    s: list,
+    lr: float = 0.001,
+    beta: float = 0.9,
+    eps: float = 1e-8,
+) -> tuple[list, list]:
+    """
+    Returns (new_w, new_s) with the same shapes as the inputs.
+    """
+    w = np.array(w)
+    g = np.array(g)
+    s = np.array(s)
+
+    new_s = beta * s + (1 - beta) * (g ** 2)
+
+    new_w = w - (lr / (np.sqrt(new_s) + eps)) * g
+
+    return new_w, new_s
